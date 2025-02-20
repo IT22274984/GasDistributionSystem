45913d97f58e359e096f1ac805da20b275f40d42
IT22274984 <127821714+IT22274984@users.noreply.github.com>
2025-02-20 21:56:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,5 @@ app.use('/api/v1/outlet', outletRoute)
 
 app.use(errorMiddleware);
 
-app.listen(PORT, () => {
-    console.log('Server is running on port ' + PORT);
-    connectToDatabase();
-});
+// Export app for serverless functions
+module.exports = app;
